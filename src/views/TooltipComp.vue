fe95892f5bf8bb64396ff9efbd2848f1c73aa7e1
sapjilmaster <sapjilmaster@gmail.com>
2024-10-01 23:15:54 +0900

    tooltip 수정
    - tooltip ES6 방식 작업

--- src/views/TooltipComp.vue
+++ src/views/TooltipComp.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
+    <p>vue에서 onMounted 전에는 dom에 접근할 수 없다.</p>
     <div
       class="tooltip"
       ref="tooltipRef"
       @mouseover="tooltip"
       @mouseleave="tooltipLeave"
     >
-      tooltip
-      <div class="inwrap" v-if="show">
+      {{ message }}
+      <div class="inwrap" ref="innerbox" v-show="show">
         {{ tooltiparea }}
       </div>
     </div>
@@ -15,28 +16,55 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { onMounted, onUnmounted, onUpdated, ref } from "vue";
 
 const tooltipRef = ref(null);
 const tooltiparea = ref(null);
+const innerbox = ref(null);
 const show = ref(false);
+const message = ref("tooltip");
 
-console.log(tooltipRef.value);
+onMounted(() => {
+  tooltipRef.value;
+  innerbox.value;
+  // console.log("onMounted: ", tooltipRef.value);
+  // console.log("onMounted: ", tooltipRef.value.clientHeight);
+});
+
+onUpdated(() => {
+  // console.log("onUpdated: ", (tooltipRef.value = message.value));
+  tooltipRef.value = message.value;
+  tooltiparea.value = "tooltiparea sample message";
+});
+
+onUnmounted(() => {
+  console.log("onUnmounted");
+});
 
 const tooltip = () => {
   tooltiparea.value = "tooltip 1";
   show.value = true;
+
+  innerbox.value.style.top = tooltipRef.value.clientHeight + 5 + "px";
+  innerbox.value.style.left = tooltipRef.value.clientWidth / 3 + "px";
 };
 
 const tooltipLeave = () => {
   tooltiparea.value = null;
   show.value = false;
+
+  innerbox.value.style.top = tooltipRef.value.clientHeight;
+  innerbox.value.style.left = tooltipRef.value.clientWidth;
 };
 </script>
 
 <style lang="scss" scoped>
 .tooltip {
   position: relative;
+  padding: 1rem;
+  margin-top: 1rem;
+  border: 1px solid salmon;
+  display: inline-block;
   .inwrap {
     position: absolute;
     background: #000;
@@ -44,7 +72,9 @@ const tooltipLeave = () => {
     border-radius: 0.5rem;
     padding: 1rem;
     left: 0;
-    top: 1rem;
+    top: 0;
+    display: block;
+    min-width: 200px;
   }
 }
 </style>
